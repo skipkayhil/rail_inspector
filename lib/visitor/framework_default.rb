629f824b8ce9961d4bb06236fd2f7d8d44c731b0
Hartley McGuire <skipkayhil@gmail.com>
2022-08-15 17:12:02 -0400

    Extract Visitor::FrameworkDefaults to lib/

---
+++ lib/visitor/framework_default.rb
@@ -0,0 +1,93 @@
+# frozen_string_literal: true
+
+require "syntax_tree"
+
+module Visitor
+  class FrameworkDefault < SyntaxTree::Visitor
+    attr_reader :config_map
+
+    def initialize
+      @config_map = {}
+      @target_version_block = false
+      @current_version = nil
+      @current_framework = nil
+    end
+
+    visit_method def visit_case(node)
+      return unless target_version_case?(node.value)
+
+      @target_version_block = true
+      visit_child_nodes(node)
+      @target_version_block = false
+    end
+
+    visit_method def visit_when(node)
+      return unless @target_version_block
+
+      @current_version = node.arguments.parts[0].parts[0].value
+
+      @config_map[@current_version] = {}
+      visit_child_nodes(node)
+
+      @current_version = nil
+    end
+
+    visit_method def visit_if(node)
+      return unless @target_version_block
+
+      @current_framework =
+        case node
+        in predicate: SyntaxTree::FCall[value: { value: "respond_to?" }]
+          node.predicate.arguments.arguments.parts[0].value.value
+        else
+          nil
+        end
+
+      visit_child_nodes(node)
+
+      @current_framework = nil
+    end
+
+    visit_method def visit_assign(node)
+      return unless @target_version_block
+
+      assert_framework(node)
+
+      target = SyntaxTree::Formatter.format(nil, node.target)
+      value =
+        case node.value
+        when SyntaxTree::StringConcat, SyntaxTree::HashLiteral
+          nil
+        else
+          SyntaxTree::Formatter.format(nil, node.value)
+        end
+      @config_map[@current_version][target] = value
+    end
+
+    private
+
+    def target_version_case?(node)
+      node in SyntaxTree::Call[
+        receiver: SyntaxTree::VarRef[
+          value: SyntaxTree::Ident[value: "target_version"]
+        ]
+      ]
+    end
+
+    def assert_framework(node)
+      framework =
+        case node.target.parent
+        in { value: SyntaxTree::Const } | { value: SyntaxTree::Kw[value: "self"] }
+          nil
+        in receiver: { value: { value: framework } }
+          framework
+        in value: { value: framework }
+          framework
+        end
+
+      return if @current_framework == framework
+
+      raise "Expected #{@current_framework} to match #{framework}"
+    end
+  end
+end
