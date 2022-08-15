5542fa98ea785194aabc4c807001d3026a6a075a
Hartley McGuire <skipkayhil@gmail.com>
2022-08-15 17:12:02 -0400

    Replace FileEditor with CachedParser and Resolver
    The cached parser will be used to pass ASTs to the check classes. Since
    checks will likely need access to the same ASTs, the parsing is cached.
    
    The interesting functionality of the RailsFileEditor was that it
    used the rails_path to get absolute paths from relative paths. That part
    was extracted into its own class, and regular File operations are now
    using the pure File methods.

--- lib/configuring.rb
+++ lib/configuring.rb
@@ -1,6 +1,38 @@
 # frozen_string_literal: true
 
 class Configuring
+  class CachedParser
+    def initialize
+      @cache = {}
+    end
+
+    def call(path)
+      @cache[path] ||= SyntaxTree.parse(SyntaxTree.read(path))
+    end
+  end
+
+  class Resolver
+    APPLICATION_CONFIGURATION_PATH =
+      "railties/lib/rails/application/configuration.rb"
+    DOC_PATH = "guides/source/configuring.md"
+
+    def initialize(rails_path)
+      @rails_path = Pathname.new(rails_path)
+    end
+
+    def call(file)
+      relative_path =
+        case file
+        in :app_config
+          APPLICATION_CONFIGURATION_PATH
+        in :doc
+          DOC_PATH
+        end
+
+      @rails_path.join(relative_path)
+    end
+  end
+
   class Doc
     attr_accessor :versioned_defaults
 
