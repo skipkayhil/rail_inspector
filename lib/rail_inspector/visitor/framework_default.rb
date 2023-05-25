2de959dfe3b4769037cb4b3c08d0eaf4968a4e89
Hartley McGuire <skipkayhil@gmail.com>
2023-05-25 18:47:27 -0400

    Extract TargetVersionCaseFinder from Visitor
    This is the first in a series of refactors to improve how
    Visitor::FrameworkDefault works. Overall, the goal is to componentize
    each part of the logic and ideally prevent errors when new code patterns
    are added to Rails::Application::Configuration.
    
    Previously, Visitor::FrameworkDefault used visit_case and an instance
    variable to control when the other visitor methods would run.
    
    While this worked fine, it makes more sense to split up the two parts:
    - TargetVersionCaseFinder is extracted as a SyntaxTree::Search, which
      performs a BFS on the AST to find the target_version case node
    - the FrameworkDefault visitor can then be simplified since it will only
      be operating within the case statement and no longer has to worry
      about any nodes outside of it

--- lib/rail_inspector/visitor/framework_default.rb
+++ lib/rail_inspector/visitor/framework_default.rb
@@ -6,94 +6,101 @@ require_relative "./hash_to_string"
 require_relative "./multiline_to_string"
 
 module Visitor
-  class FrameworkDefault < SyntaxTree::Visitor
+  class FrameworkDefault
+    TargetVersionCaseFinder =
+      SyntaxTree::Search.new(
+        ->(node) do
+          node in SyntaxTree::Case[
+            value: SyntaxTree::CallNode[
+              receiver: SyntaxTree::VarRef[
+                value: SyntaxTree::Ident[value: "target_version"]
+              ]
+            ]
+          ]
+        end
+      )
+
     attr_reader :config_map
 
     def initialize
       @config_map = {}
-      @target_version_block = false
-      @current_version = nil
-      @current_framework = nil
     end
 
-    visit_method def visit_case(node)
-      return unless target_version_case?(node.value)
+    def visit(node)
+      case_node, *others = TargetVersionCaseFinder.scan(node).to_a
+      raise "#{others.length} other cases?" unless others.empty?
 
-      @target_version_block = true
-      visit_child_nodes(node)
-      @target_version_block = false
+      internal_visitor = FrameworkDefaultInternal.new
+      internal_visitor.visit(case_node)
+      @config_map = internal_visitor.config_map
     end
 
-    visit_method def visit_when(node)
-      return unless @target_version_block
+    class FrameworkDefaultInternal < SyntaxTree::Visitor
+      attr_reader :config_map
 
-      @current_version = node.arguments.parts[0].parts[0].value
+      def initialize
+        @config_map = {}
+        @current_version = nil
+        @current_framework = nil
+      end
 
-      @config_map[@current_version] = {}
-      visit_child_nodes(node)
+      visit_method def visit_when(node)
+        @current_version = node.arguments.parts[0].parts[0].value
 
-      @current_version = nil
-    end
+        @config_map[@current_version] = {}
+        visit_child_nodes(node)
 
-    visit_method def visit_if(node)
-      return unless @target_version_block
+        @current_version = nil
+      end
 
-      @current_framework =
-        case node
-        in predicate: SyntaxTree::CallNode[message: { value: "respond_to?" }]
-          node.predicate.arguments.arguments.parts[0].value.value
-        else
-          nil
-        end
+      visit_method def visit_if(node)
+        @current_framework =
+          case node
+          in predicate: SyntaxTree::CallNode[message: { value: "respond_to?" }]
+            node.predicate.arguments.arguments.parts[0].value.value
+          else
+            nil
+          end
 
-      visit_child_nodes(node)
+        visit_child_nodes(node)
 
-      @current_framework = nil
-    end
+        @current_framework = nil
+      end
 
-    visit_method def visit_assign(node)
-      return unless @target_version_block
+      visit_method def visit_assign(node)
+        assert_framework(node)
 
-      assert_framework(node)
+        target = SyntaxTree::Formatter.format(nil, node.target)
+        value =
+          case node.value
+          when SyntaxTree::HashLiteral
+            HashToString.new.tap { |v| v.visit(node.value) }.to_s
+          when SyntaxTree::StringConcat
+            MultilineToString.new.tap { |v| v.visit(node.value) }.to_s
+          else
+            SyntaxTree::Formatter.format(nil, node.value)
+          end
+        @config_map[@current_version][target] = value
+      end
 
-      target = SyntaxTree::Formatter.format(nil, node.target)
-      value =
-        case node.value
-        when SyntaxTree::HashLiteral
-          HashToString.new.tap { |v| v.visit(node.value) }.to_s
-        when SyntaxTree::StringConcat
-          MultilineToString.new.tap { |v| v.visit(node.value) }.to_s
-        else
-          SyntaxTree::Formatter.format(nil, node.value)
-        end
-      @config_map[@current_version][target] = value
-    end
+      private
 
-    private
+      def assert_framework(node)
+        framework =
+          case node.target.parent
+          in { value: SyntaxTree::Const } |
+               { value: SyntaxTree::Kw[value: "self"] }
+            nil
+          in receiver: { value: { value: framework } }
+            framework
+          in value: { value: framework }
+            framework
+          end
 
-    def target_version_case?(node)
-      node in SyntaxTree::CallNode[
-        receiver: SyntaxTree::VarRef[
-          value: SyntaxTree::Ident[value: "target_version"]
-        ]
-      ]
-    end
+        return if @current_framework == framework
 
-    def assert_framework(node)
-      framework =
-        case node.target.parent
-        in { value: SyntaxTree::Const } |
-             { value: SyntaxTree::Kw[value: "self"] }
-          nil
-        in receiver: { value: { value: framework } }
-          framework
-        in value: { value: framework }
-          framework
-        end
-
-      return if @current_framework == framework
-
-      raise "Expected #{@current_framework} to match #{framework}"
+        raise "Expected #{@current_framework} to match #{framework}"
+      end
     end
   end
 end
