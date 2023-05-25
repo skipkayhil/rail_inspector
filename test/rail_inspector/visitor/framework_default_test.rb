512636f2f217ac1afc87e661f19207efa2fcf6aa
Hartley McGuire <skipkayhil@gmail.com>
2023-05-25 18:47:26 -0400

    Add tests for Visitor::FrameworkDefault
    The smoke test covers all of the basic ways that configuration are set:
    - framework.config
    - Class.config
    - self.config

---
+++ test/rail_inspector/visitor/framework_default_test.rb
@@ -0,0 +1,72 @@
+# frozen_string_literal: true
+
+require "visitor/framework_default"
+
+module Visitor
+  class FrameworkDefaultTest < Minitest::Test
+    def test_smoke
+      config = config_for_defaults <<~RUBY
+        case target_version.to_s
+        when "5.0"
+          ActiveSupport.to_time_preserves_timezone = true
+
+          if respond_to?(:active_record)
+            active_record.belongs_to_required_by_default = true
+          end
+
+          self.ssl_options = { hsts: { subdomains: true } }
+        when "5.1"
+          load_defaults "5.0"
+
+          if respond_to?(:assets)
+            assets.unknown_asset_fallback = false
+          end
+        end
+      RUBY
+
+      ["5.0", "5.1"].each { |k| assert_includes(config, k) }
+
+      assert_equal config["5.0"]["ActiveSupport.to_time_preserves_timezone"], "true"
+      assert_equal config["5.0"]["active_record.belongs_to_required_by_default"], "true"
+      assert_equal config["5.0"]["self.ssl_options"], "{ hsts: { subdomains: true } }"
+      assert_equal config["5.1"]["assets.unknown_asset_fallback"], "false"
+    end
+
+    def test_config_wrapped_in_condition
+      config = config_for_defaults <<~RUBY
+        case target_version.to_s
+        when "7.1"
+          if Rails.env.local?
+            self.log_file_size = 100 * 1024 * 1024
+          end
+        end
+      RUBY
+
+      assert_includes config, "7.1"
+      assert_equal config["7.1"]["self.log_file_size"], "100 * 1024 * 1024"
+    end
+
+    private
+
+    def wrapped_defaults(defaults)
+      <<~RUBY
+        class Configuration
+          def load_defaults(target_version)
+            #{defaults}
+          end
+        end
+      RUBY
+    end
+
+    def config_for_defaults(defaults)
+      full_class = wrapped_defaults(defaults)
+      parsed = SyntaxTree.parse(full_class)
+      visitor.visit(parsed)
+      visitor.config_map
+    end
+
+    def visitor
+      @visitor ||= Visitor::FrameworkDefault.new
+    end
+  end
+end
