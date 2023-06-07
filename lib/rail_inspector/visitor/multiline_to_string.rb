e7eba2a8896db1d8b63e49fd38118e5909dc8a64
Hartley McGuire <skipkayhil@gmail.com>
2023-06-06 21:07:27 -0400

    Move Visitors under RailInspector module

--- lib/rail_inspector/visitor/multiline_to_string.rb
+++ lib/rail_inspector/visitor/multiline_to_string.rb
@@ -2,23 +2,25 @@
 
 require "syntax_tree"
 
-module Visitor
-  class MultilineToString < SyntaxTree::Visitor
-    attr_reader :to_s
+module RailInspector
+  module Visitor
+    class MultilineToString < SyntaxTree::Visitor
+      attr_reader :to_s
 
-    def initialize
-      @to_s = +""
-    end
-
-    visit_methods do
-      def visit_string_concat(node)
-        @to_s << '"'
-        super(node)
-        @to_s << '"'
+      def initialize
+        @to_s = +""
       end
 
-      def visit_tstring_content(node)
-        @to_s << node.value
+      visit_methods do
+        def visit_string_concat(node)
+          @to_s << '"'
+          super(node)
+          @to_s << '"'
+        end
+
+        def visit_tstring_content(node)
+          @to_s << node.value
+        end
       end
     end
   end
